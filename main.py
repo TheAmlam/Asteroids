54baef3f0efb81851a056f819ccd5bc4e74bfbc4
TheAmlam <mariolozano.0903@gmail.com>
2024-08-24 13:53:51 +0200

    added the ability to turn

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ def main ():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
-    game_loop(screen, clock, player)
+    game_loop(screen, clock, player, dt)
 
-def game_loop(screen, clock, player):
+def game_loop(screen, clock, player, dt):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
